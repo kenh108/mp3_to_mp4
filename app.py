e37c0759420452cdf612007db7c9dc002f9aef8e
Kenneth Heeney <kenneth@heeney.ie>
2025-01-30 21:12:30 +0000

    if file is too large error message is shown

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ ALLOWED_IMAGE_TYPES = {'png', 'jpg', 'jpeg'}
 ALLOWED_AUDIO_TYPES = {'mp3'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 # 16mb limit
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename, allowed_extensions):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+@app.route('/config')
+def get_config():
+    return jsonify({"max_file_size": app.config['MAX_CONTENT_LENGTH']})
 
 @app.route('/')
 def home():
@@ -21,19 +27,19 @@ def home():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'image' not in request.files or 'audio' not in request.files:
-        return "image and mp3 required", 400
+        return jsonify({"message": "both image and mp3 required"}), 400
 
     image = request.files['image']
     audio = request.files['audio']
 
     if image.filename == '' or audio.filename == '':
-        return "image and mp3 required", 400
+        return jsonify({"message": "both files not selected"}), 400
 
     if not allowed_file(image.filename, ALLOWED_IMAGE_TYPES):
-        return "invalid image format", 400
+        return jsonify({"message": "invalid image format; only png, jpg, jpeg accepted"}), 400
 
     if not allowed_file(audio.filename, ALLOWED_AUDIO_TYPES):
-        return "invalid audio format", 400
+        return jsonify({"message": "invalid audio format; only mp3 accepted"}), 400
 
     unique_image_filename = str(uuid.uuid4()) + os.path.splitext(image.filename)[1]
     unique_audio_filename = str(uuid.uuid4()) + os.path.splitext(audio.filename)[1]
@@ -46,6 +52,5 @@ def upload_file():
 
     return jsonify({"message": f"files uploaded successfully: {image.filename}, {audio.filename}"})
 
-        
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
