8bb15f07d816c1f750ed2d52d22b1213cdc6fa6a
Kenneth Heeney <kenneth@heeney.ie>
2025-02-01 13:53:34 +0000

    added timeouts with error message for image and video processing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 import subprocess
+import time
 from flask import Flask, request, render_template, redirect, url_for, jsonify, send_from_directory, send_file
 from PIL import Image
 from mutagen.mp3 import MP3
@@ -12,35 +13,18 @@ UPLOAD_FOLDER='uploads'
 OUTPUT_FOLDER='processed'
 ALLOWED_IMAGE_TYPES = {'png', 'jpg', 'jpeg', 'webp'}
 ALLOWED_AUDIO_TYPES = {'mp3'}
+FFMPEG_IMAGE_TIMEOUT = 5
+FFMPEG_VIDEO_TIMEOUT = 30
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER 
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 # 16mb limit
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 # 50mb limit
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-# Ensure file types are supported
 def allowed_file(filename, allowed_extensions):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
-
-# Ensure height and width are divisible by 2 for ffmpeg
-def ensure_even_dimensions(image_path):
-    img = Image.open(image_path)
-    width, height = img.size
-
-    if width % 2 == 0 and height % 2 == 0:
-        return image_path
-
-    new_width = width - (width % 2)
-    new_height = height - (height % 2)
-
-    img = img.resize((new_width, new_height), Image.LANCZOS)
-    new_image_path = image_path.replace(".", "_even.")
-    
-    img.save(new_image_path)
-
-    return new_image_path
 
 def get_audio_duration(audio_path):
     audio = MP3(audio_path)
@@ -97,7 +81,10 @@ def upload_files():
     ]
 
     try:
-        subprocess.run(ffmpeg_image_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        start_time = time.time()
+        subprocess.run(ffmpeg_image_command, check=True, timeout=FFMPEG_IMAGE_TIMEOUT, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.TimeoutExpired:
+        return jsonify({"message": "processing cancelled due to image conversion timeout"})
     except subprocess.CalledProcessError as e:
         return jsonify({"message": f"ffmpeg error processing image: {e.stderr.decode()}"}), 500
 
@@ -113,7 +100,10 @@ def upload_files():
     ]
 
     try:
-        subprocess.run(ffmpeg_video_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        start_time = time.time()
+        subprocess.run(ffmpeg_video_command, check=True, timeout=FFMPEG_VIDEO_TIMEOUT, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.TimeoutExpired:
+        return jsonify({"message": "processing cancelled due to video conversion timeout"})
     except subprocess.CalledProcessError as e:
         return jsonify({"message": f"ffmpeg error processing video: {e.stderr.decode()}"}), 500
 
